6c1fe5dafbad1ddad1d278c84e60b21ab4bb9900
Arthurisme <is@live.ca>
2015-08-19 23:03:17 -0400

    Initial commit v0.01

---
+++ myapp/api.py
@@ -0,0 +1,10 @@
+__author__ = 'Arthur'
+
+from tastypie.resources import ModelResource
+from myapp.models import Entry
+
+
+class EntryResource(ModelResource):
+    class Meta:
+        queryset = Entry.objects.all()
+        resource_name = 'entry'
