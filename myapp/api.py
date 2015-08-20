6e44ba3074c031008681e37877067079870e2fc7
Arthurisme <is@live.ca>
2015-08-19 23:25:47 -0400

    v0.02

--- myapp/api.py
+++ myapp/api.py
@@ -1,10 +1,24 @@
 __author__ = 'Arthur'
 
+
+
+from tastypie.authorization import Authorization
+from django.contrib.auth.models import User
+from tastypie import fields
 from tastypie.resources import ModelResource
 from myapp.models import Entry
 
 
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user'
+
+
 class EntryResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'user')
+
     class Meta:
         queryset = Entry.objects.all()
         resource_name = 'entry'
+        authorization= Authorization()
